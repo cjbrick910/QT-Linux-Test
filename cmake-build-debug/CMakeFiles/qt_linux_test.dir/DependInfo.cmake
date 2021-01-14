0307882951dd2c69e88a9f176aaceadc3389e428
cjbrick910 <chris@housetech.guru>
2021-01-13 22:18:13 -0500

    testing

---
+++ cmake-build-debug/CMakeFiles/qt_linux_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lenin/git/qt-linux-test/main.cpp" "/home/lenin/git/qt-linux-test/cmake-build-debug/CMakeFiles/qt_linux_test.dir/main.cpp.o"
+  "/home/lenin/git/qt-linux-test/moc_program.cpp" "/home/lenin/git/qt-linux-test/cmake-build-debug/CMakeFiles/qt_linux_test.dir/moc_program.cpp.o"
+  "/home/lenin/git/qt-linux-test/program.cpp" "/home/lenin/git/qt-linux-test/cmake-build-debug/CMakeFiles/qt_linux_test.dir/program.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/qt_linux_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qt_linux_test.dir/main.cpp.o"
+  "CMakeFiles/qt_linux_test.dir/moc_program.cpp.o"
+  "CMakeFiles/qt_linux_test.dir/program.cpp.o"
+  "qt_linux_test"
+  "qt_linux_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qt_linux_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
